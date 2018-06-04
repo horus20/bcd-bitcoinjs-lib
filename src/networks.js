782ce8ae6ca82ba2acc617a5a31daf456507f736
Starry9 <kenvpn@gmail.com>
2018-06-04 11:29:36 +0800

    Support bcd transaction

--- src/networks.js
+++ src/networks.js
@@ -33,5 +33,16 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0
+  },
+  bitcoindiamond: {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x00,
+    scriptHash: 0x05,
+    wif: 0x80
   }
 }
